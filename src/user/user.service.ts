94ad4396f11d9bde81e4baeb548f7261993c4d9c
Volha Patsiahevich <zaprudskaya@gmail.com>
2024-11-06 15:09:19 +0100

    fix: fix user interface

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,13 +2,11 @@ import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdatePasswordDto } from './dto/update-password.dto';
 import { v4 as uuid } from 'uuid';
+import { User } from './user.interface';
 
 @Injectable()
 export class UserService {
-  private users = new Map<
-    string,
-    { id: string; login: string; password: string }
-  >();
+  private users = new Map<string, User>();
 
   findAll() {
     return Array.from(this.users.values());
@@ -20,7 +18,10 @@ export class UserService {
 
   create(createUserDto: CreateUserDto) {
     const id = uuid();
-    const newUser = { id, ...createUserDto };
+    const createdAt = new Date().getTime();
+    const updatedAt = new Date().getTime();
+    const version = 1;
+    const newUser = { id, createdAt, updatedAt, version, ...createUserDto };
     this.users.set(id, newUser);
     return newUser;
   }
@@ -37,6 +38,7 @@ export class UserService {
     }
 
     user.password = updatePasswordDto.newPassword;
+    user.updatedAt = new Date().getTime();
     this.users.set(id, user);
     return user;
   }
